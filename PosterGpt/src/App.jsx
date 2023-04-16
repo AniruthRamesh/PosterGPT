90d0ee509c3b144bcbbeab3ddde3a76e4e780c46
AniruthRamesh <anikrishna3@gmail.com>
2023-04-16 17:32:45 -0400

    finalized client page

--- PosterGpt/src/App.jsx
+++ PosterGpt/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   const [prompt,setPrompt] = useState("")
   const [email,setEmail] = useState("")
   const [toggle,setToggle] = useState(false)
+  const [submit,setSubmit] = useState(false)
 
 
   const handleVibe = (option)=>{
@@ -25,8 +26,12 @@ function App() {
 
    const handleSubmit = async ()=>{
 
-    
-    const response = await axios.post("http://localhost:4000/api/prompts",{})
+    setSubmit(true);
+    const response = await axios.post("https://postergptserver.onrender.com/api/prompts",{House:house, Vibe:vibe, character:character, prompt:prompt, emailid:email})
+    if(response.status==200)
+      {
+        console.log(response)
+      }
    }
 
    const handleToggle = ()=>{
@@ -45,8 +50,8 @@ function App() {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-6">
-          <div className="container p-5 text-light" style={{marginLeft:"10rem"}}>
-          <TypeAnimation className="text-light"
+          <div className="container m-1 p-4 text-light text-center" style={{marginLeft:"10rem", color:"black"}}>
+          <TypeAnimation className="text-dark fw-bold"
           sequence={[
             'posterGPT', // Types 'One'
             2000, // Waits 1s
@@ -101,14 +106,19 @@ function App() {
                 <form>
                   <div className="form-group mt-4">
                     <label htmlFor="promptInput text-light" style={{color:"white"}}>Prompt</label>
-                    <input type="text" className="form-control rounded-4" id="promptInput" placeholder="i want a cool looking monkey in anime style" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+                    <input type="text" className="form-control rounded-4" id="promptInput" placeholder="Lofi Naruto in futuristic Shinobi Village" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
                   </div>
                   <div className="form-group mt-4">
                     <label htmlFor="emailInput" style={{color:"white"}}>Email</label>
                     <input type="email" className="form-control rounded-4" id="emailInput" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
                   </div>
                   <button className="btn btn-dark rounded-4 mt-4" type="button" onClick={handleSubmit}>Generate</button>
+                  {submit && <><p className='text-light fw-bold fs-4 font-weight-bold mt-3' style={{fontSize:"20px; font"}}>Request Submitted</p></>}
                 </form>
+
+
+
+               
 
 
 
@@ -116,6 +126,76 @@ function App() {
           </div>
         </div>
       </div>
+
+
+
+<div className='mt-5'>
+      <h3 className='text-center mb-4' style={{ color: 'black' }}>
+
+      <TypeAnimation className="text-light fw-bold"
+          sequence={[
+            'Sample ', // Types 'One'
+            2000, // Waits 1s
+            '', // Deletes 'One' and types 'Two'
+            () => {
+              console.log('Sequence completed'); // Place optional callbacks anywhere in the array
+            }
+          ]}
+          wrapper="span"
+          cursor={true}
+          repeat={Infinity}
+          style={{ fontSize: '0.8em', display: 'inline-block',fontFamily:"Courier New", }}
+        />
+
+      </h3>
+      <div className="row mt-5 d-flex flex-row justify-content-center align-items-center">
+      <div className="col-md-3">
+        <div className="card" style={{borderColor:'black'}}>
+          <img src="./images/narutocy.png" className="card-img-top card-img" alt="placeholder" />
+          <div className="card-body" style={{backgroundColor:'black'}}>
+            <h5 className="card-title text-center">CyberPunk Naruto</h5>
+            
+          </div>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card" style={{borderColor:'black'}}>
+          <img src="./images/goku.png" className="card-img-top card-img" alt="placeholder" />
+          <div className="card-body" style={{backgroundColor:'black'}}>
+            <h5 className="card-title text-center">Lofi Goku</h5>
+        
+          </div>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card" style={{borderColor:'black'}}>
+          <img src="./images/tanjiro_kamado.png" className="card-img-top card-img" alt="placeholder" />
+          <div className="card-body" style={{backgroundColor:'black'}}>
+            <h5 className="card-title text-center">Vin Kamado</h5>
+          
+          </div>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card" style={{borderColor:'black'}}>
+          <img src="./images/luffy.png" className="card-img-top card-img" alt="placeholder" />
+          <div className="card-body" style={{backgroundColor:'black'}}>
+            <h5 className="card-title text-center">WW-II Luffy</h5>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
     </div>
 
     </div>
